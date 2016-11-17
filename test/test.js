4a9184d5528a8fd593772f42a0e06ded13b7ede3
maasencioh <maasencioh@gmail.com>
2016-11-17 11:43:26 -0500

    test: increase coverage

--- test/test.js
+++ test/test.js
@@ -1,59 +1,60 @@
 'use strict';
 
-var padArray = require('..');
+const padArray = require('..');
+const data = [1, 2, 3, 4];
 
 describe('Array test', function () {
 
     it('Default test', function () {
-        var data = [1, 2, 3, 4];
-        var model = padArray(data);
-        model.should.eql([0, 1, 2, 3, 4, 0]);
+        padArray(data).should.eql([0, 1, 2, 3, 4, 0]);
     });
 
     it('Output option test', function () {
-        var data = [1, 2, 3, 4];
-        var model = new Array(6);
-        padArray(data, {output: model});
-        model.should.eql([0, 1, 2, 3, 4, 0]);
+        let model = new Array(6);
+        padArray(data, {output: model}).should.eql([0, 1, 2, 3, 4, 0]);
     });
 
     it('Replicate test', function () {
-        var data = [1, 2, 3, 4];
-        var options = {
+        let options = {
             size: 3,
             value: 'replicate'
         };
-        var model = padArray(data, options);
-        model.should.eql([1, 1, 1, 1, 2, 3, 4, 4, 4, 4]);
+        padArray(data, options).should.eql([1, 1, 1, 1, 2, 3, 4, 4, 4, 4]);
     });
 
     it('Circular test', function () {
-        var data = [1, 2, 3, 4];
-        var options = {
+        let options = {
             size: 5,
             value: 'circular'
         };
-        var model = padArray(data, options);
-        model.should.eql([4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1]);
+        padArray(data, options).should.eql([4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1]);
     });
 
     it('Symmetric test', function () {
-        var data = [1, 2, 3, 4];
-        var options = {
+        let options = {
             size: 3,
             value: 'symmetric'
         };
-        var model = padArray(data, options);
-        model.should.eql([3, 2, 1, 1, 2, 3, 4, 4, 3, 2]);
+        padArray(data, options).should.eql([3, 2, 1, 1, 2, 3, 4, 4, 3, 2]);
     });
 
     it('Numeric test', function () {
-        var data = [1, 2, 3, 4];
-        var options = {
+        let options = {
             size: 3,
             value: 8
         };
-        var model = padArray(data, options);
-        model.should.eql([8, 8, 8, 1, 2, 3, 4, 8, 8, 8]);
+        padArray(data, options).should.eql([8, 8, 8, 1, 2, 3, 4, 8, 8, 8]);
+    });
+
+    it('Errors test', function () {
+        padArray.bind(null, 4).should.throw('data should be an array');
+        padArray.bind(null, [[1]]).should.throw('matrix not supported yet, sorry');
+        padArray.bind(null, data, {output: []}).should.throw('wrong output size');
+
+        let symmetric = {
+            size: [8, 2],
+            value: 'symmetric'
+        };
+        padArray.bind(null, data, symmetric).should.throw('expanded value should not be bigger than the data length');
     });
 });
